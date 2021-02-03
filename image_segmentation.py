44cf57fbed2e9d0725e76d9a1b61529222fdb279
Lucas <pagano.lucas@hotmail.com>
2021-02-03 13:19:23 -0800

    debug print

--- image_segmentation.py
+++ image_segmentation.py
@@ -4,15 +4,29 @@ import numpy as np
 from torchvision import models, transforms
 from torchvision.datasets import Cityscapes
 
+
+def labels():
+    l = {}
+    for i, label in enumerate(classes):
+        l[i] = label
+    return l
+
+
+def wb_mask(bg_img, pred_mask, true_mask):
+    return wandb.Image(bg_img, masks={
+        "prediction": {"mask_data": pred_mask, "class_labels": labels()},
+        "ground truth": {"mask_data": true_mask, "class_labels": labels()}})
+
+
 ## wandb
 run = wandb.init(project="wandb-demo")
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
 fcn = models.segmentation.fcn_resnet101(pretrained=True).eval().to(device)
 
-classes = {'__background__', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
+classes = ['__background__', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
            'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike',
-           'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'}
+           'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
 
 transform = transforms.Compose([
     transforms.ToTensor(),
@@ -28,6 +42,7 @@ cpt = 0
 for data, image in data_iter:
     if cpt < 1:
         batch_images = data.to(device)
+        print(batch_images)
         out = fcn(batch_images.unsqueeze(0))
         for key, value in out.items():
             print(key)
@@ -36,4 +51,5 @@ for data, image in data_iter:
                 print(value.size())
             except:
                 pass
+        to_log = wb_mask(batch_images)
         cpt += 1
