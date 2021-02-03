d474df29322ea0491539c4b78c06d2eb89459d91
Lucas <pagano.lucas@hotmail.com>
2021-02-03 15:00:30 -0800

    fix image

--- image_segmentation.py
+++ image_segmentation.py
@@ -38,7 +38,7 @@ mean = [0.485, 0.456, 0.406]
 m_mean = [-i for i in mean]
 std = [0.229, 0.224, 0.225]
 std_inv = [1 / i for i in std]
-mean_inv = m_mean * std_inv
+mean_inv = [i * j for i, j in zip(m_mean, std_inv)]
 unorm = transforms.Normalize(mean=mean_inv, std=std_inv)
 
 dataset = Cityscapes('./datasets/Cityscapes/', split='val', mode='fine',
