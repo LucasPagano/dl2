b03f5ce7eb5e0a4b4178da569eb9fe36fb1b2783
Lucas <pagano.lucas@hotmail.com>
2021-02-08 12:18:30 -0800

    debug prints

--- zoo.py
+++ zoo.py
@@ -89,8 +89,10 @@ class BVAE(nn.Module):
 
     def forward(self, x):
         encoded = self.encode(x)
+        print("Encoded : [{}; {}]".format(encoded.min(), encoded.max()))
         mu, log_var = encoded[:, :self.z_dim], encoded[:, self.z_dim:]
         z = self.sampling(mu, log_var)
+        print("z : [{}; {}]".format(z.min(), z.max()))
         x_recon = self.decode(z)
         return x_recon, mu, log_var
 
