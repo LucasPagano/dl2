a49d2c9ef1ec6c6ac8da9251900ee985a6a65567
Lucas <pagano.lucas@hotmail.com>
2021-03-05 23:34:41 -0800

    bugfix

--- zoo.py
+++ zoo.py
@@ -117,7 +117,7 @@ class BVAE(nn.Module):
         return x_recon, mu, log_var
 
     def get_loss(self, recon_x, x, mu, log_var):
-        bce = F.binary_cross_entropy(recon_x.view(-1, 32 * 32), x.view(-1, 32 * 32))
+        bce = F.binary_cross_entropy(recon_x.view(-1, 32 * 32), x.view(-1, 32 * 32), reduction="sum")
         # mse = torch.nn.MSELoss()(recon_x.view(x.size()), x)
         kld = -0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim=1)
         # info-vae part
